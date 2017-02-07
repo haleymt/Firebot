eda13a3bd1ce8aa56090441601fed14041458230
Haley <haleymthurston@gmail.com>
2017-02-06 18:31:48 -0800

    button styling

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/oauth', function(req, res, next) {
+  res.render('success');
+});
+
 module.exports = router;
