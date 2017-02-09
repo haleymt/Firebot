6b23a75e8e55f11870de850fdb1f60ca57f23015
Haley <haleymthurston@gmail.com>
2017-02-09 14:43:53 -0800

    handle user join leave

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var { hostName } = require('../constants');
+var { hostName } = require('../lib/constants');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
